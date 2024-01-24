cd4d7f2952724c171ee8daab0ffdf123985799d4
Sumit <sumitgarg380@gmail.com>
2024-01-25 00:43:01 +0530

    User Profile and Members Sidebar

--- app/(main)/servers/[servers]/layout.jsx
+++ app/(main)/servers/[servers]/layout.jsx
@@ -10,18 +10,23 @@ export default async function ChannelLayout({children, params}){
         return redirectToSignIn();
     }
     const server = await Server.findOne({_id:params.servers})
+    const users = await Server.findOne({_id:params.servers}).populate('users');
+    console.log(users)
     if(!server){
         return redirect('/');
     }
 
     return(
-        <div className="min-h-screen w-full">
+        <div className="min-h-screen w-full flex  justify-between relative">
             <div className="min-h-screen flex flex-col fixed w-60">
                 <ServerSideBar name={server.servername} code={server.inviteCode} param={params.servers} />
             </div>
             <main className="md:pl-60 min-h-screen">
                 {children}
             </main>
+            <div className="min-h-screen flex flex-col w-60 bg-[#2b2d31] sticky items-center py-4">
+                hehe
+            </div>
         </div>
     )
 }
--- components/ServerSideBar.jsx
+++ components/ServerSideBar.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import Modal from './InviteModal'
-import Dropdown from './DropDown'
-import { Channel } from '@/models/channelModel'
-import { FetchChannel } from '@/lib/fetch'
+import React from "react";
+import Modal from "./InviteModal";
+import Dropdown from "./DropDown";
+import { Channel } from "@/models/channelModel";
+import { FetchChannel } from "@/lib/fetch";
+import UserProfile from "./UserProfile";
 
-const ServerSideBar = async ({name, code,param}) => {
-   const TextChannels=await FetchChannel();
-   
+const ServerSideBar = async ({ name, code, param }) => {
+  const TextChannels = await FetchChannel();
+
   return (
-    <div className='flex flex-col bg-[#2b2d31] w-full min-h-screen p-2'>
-        <Dropdown name={name} code={code} param={param} />
-          {TextChannels.length>0 && (
-            <div className='text-sm mt-6 text-zinc-300 font-semibold'>
-                TEXT CHANNELS
-            </div>
-          )}
-          {TextChannels.map((item)=>(
-           <button key={item._id} className='p-2 mb-3 text-center font-sans  bg-[#3c3c3c] text-large  '>
-                #{item.channelName}
-           </button>
-          ))}
+    <div className="flex flex-col bg-[#2b2d31] w-full min-h-screen p-2 justify-between">
+      <Dropdown name={name} code={code} param={param} />
+      <div className="w-full h-full flex flex-col justify-start">
+        {TextChannels.length > 0 && (
+          <div className="text-sm mt-6 text-zinc-300 font-semibold">
+            TEXT CHANNELS
+          </div>
+        )}
+        {TextChannels.map((item) => (
+          <button
+            key={item._id}
+            className="p-2 mb-3 text-center font-sans  bg-[#3c3c3c] text-large  "
+          >
+            #{item.channelName}
+          </button>
+        ))}
+      </div>
+      <UserProfile />
     </div>
-  )
-}
+  );
+};
 
-export default ServerSideBar
+export default ServerSideBar;
