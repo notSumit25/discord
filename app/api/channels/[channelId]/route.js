48e2f2857498634b2fcd1bc975ad03a6d9c5eae9
Lernobug <gunjanaggarwal12345.com>
2024-01-27 20:56:03 +0530

    edit and delete channel done

--- app/api/channels/[channelId]/route.js
+++ app/api/channels/[channelId]/route.js
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs";
 import { User } from "@/models/userModel.js";
 import { Server } from "@/models/serverModel";
+import { Prata } from "next/font/google";
 
 await connect();
 
@@ -28,48 +29,21 @@ export async function DELETE(req,{params})
 export async function PATCH(req,{params}){
     try {
         const { name, type } = await req.json();
+        console.log(name,type);
         const user=await currentUser();
-        const serverId = req.query.serverId; 
         const userm=await User.findOne({userId:user.id})
         if(!userm)
         {
             return new NextResponse.json({message:"Unauthorized"}, { status: 401 }); 
         }
-        if(!serverId)
-        {
-            return new NextResponse("Server ID missing", { status: 400 });
-        }
         if (!params.channelId) {
             return new NextResponse("Channel ID missing", { status: 400 });
           }
-          const isAuthorized = await Server.exists({
-            _id: serverId,
-            users: {
-              $elemMatch: {
-                userId: userm._id,
-                role: { $in: ["ADMIN","MODERATOR"] }
-              }
-            }
-          });
-          if (!isAuthorized) {
-            return new NextResponse("Unauthorized", { status: 401 });
-          }
-
-        const server = await Server.findByIdAndUpdate(
-       {
-        _id: serverId,
-        "channels._id": params.channelId,
-        "channels.name": { $ne: "Member" }
-       },
-        {
-          $set: {
-            "channels.$.Channelname": name,
-            "channels.$.type": type
-          }
-        },
-        { new: true }
-      );
-      return NextResponse.json(server);
+         const channel =await Channel.findByIdAndUpdate(params.channelId,{
+          channelName:name,
+          type:type,
+         })
+      return NextResponse.json(channel);
     } catch (error) {
         console.log("[CHANNEL_UPDATED]", error);
         return new NextResponse("Internal Error", { status: 500 });
