01d929a8b10d931443169fd0dad953a9076cc84f
Sumit <sumitgarg380@gmail.com>
2024-02-04 20:01:01 +0530

    Layout

--- app/(main)/servers/[servers]/layout.jsx
+++ app/(main)/servers/[servers]/layout.jsx
@@ -2,6 +2,7 @@ import ServerSideBar from "@/components/ServerSideBar";
 import { currentUsers } from "@/lib/currentUser";
 import { Server } from "@/models/serverModel";
 import { redirectToSignIn } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function ChannelLayout({children, params}){
@@ -9,15 +10,22 @@ export default async function ChannelLayout({children, params}){
     if(!profile){
         return redirectToSignIn();
     }
-    
     const server = await Server.findOne({_id:params.servers})
     const users = await Server.findOne({_id:params.servers}).populate('users.userId');
     const usersinfo=await users.users;
-    const member=usersinfo.filter(user=> user.role =='Member')
-    const Member = member.map(user => user.userId.username);
-    const admin=usersinfo.filter(user=> user.role =='Admin')
-    const Admin = admin.map(user => user.userId.username);
-    console.log(Member)
+    const member = usersinfo.filter(user => user.role === 'Member');
+    const Member = member.map(user => ({
+        username: user.userId.username,
+        pic: user.userId.pic,
+        userId: user.userId._id,
+    }));
+    const admin = usersinfo.filter(user => user.role === 'Admin');
+    const Admin = admin.map(user => ({
+        username: user.userId.username,
+        pic: user.userId.pic,
+        userId: user.userId._id,
+    }));
+    // console.log(Member)
     if(!server){
         return redirect('/');
     }
@@ -30,29 +38,30 @@ export default async function ChannelLayout({children, params}){
             <main className="md:pl-60 min-h-screen">
                 {children}
             </main>
-            <div className="min-h-screen flex flex-col w-60 bg-[#2b2d31] sticky items-start py-4 px-4 text-white">
+            <div className="min-h-screen flex flex-col w-60 bg-[#2b2d31] fixed right-0 items-start py-4 px-4 z-50 text-white">
+            {Admin.length > 0 && (
+                    <div className="my-4">
+                        <h2 className="text-lg font-bold mb-2">Admin</h2>
+                        {Admin.map((item) => (
+                            <div key={item.userId} className="flex items-center mb-2">
+                                <Image width={100} height={100} src={item.pic} className="h-8 w-8 bg-gray-500 rounded-full mr-2" />
+                                <div>{item.username}</div>
+                            </div>
+                        ))}
+                    </div>
+                )}
                 {Member.length > 0 && (
                     <div>
                         <h2 className="text-lg font-bold mb-2">Members</h2>
                         {Member.map((item) => (
-                            <div key={item} className="flex items-center mb-2">
-                                <div className="h-8 w-8 bg-gray-500 rounded-full mr-2"></div>
-                                <div>{item}</div>
+                            <div key={item.userId} className="flex items-center mb-2">
+                                <Image width={100} height={100} src={item.pic} className="h-8 w-8 bg-gray-500 rounded-full mr-2" />
+                                <div>{item.username}</div>
                             </div>
                         ))}
                     </div>
                 )}
-                {Admin.length > 0 && (
-                    <div className="mt-4">
-                        <h2 className="text-lg font-bold mb-2">Admin</h2>
-                        {Admin.map((item) => (
-                            <div key={item} className="flex items-center mb-2">
-                                <div className="h-8 w-8 bg-gray-500 rounded-full mr-2"></div>
-                                <div>{item}</div>
-                            </div>
-                        ))}
-                    </div>
-                )}
+                
             </div>
             
         </div>
