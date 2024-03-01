6828fdf707f16e5cc61a8a7479c492e4dbff2e68
Sumit <sumitgarg380@gmail.com>
2024-03-01 12:39:02 +0530

    Fixed some things

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import ScrollableFeed from "react-scrollable-feed";
 import { io } from "socket.io-client";
--- components/EditServer.jsx
+++ components/EditServer.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import { UploadButton, UploadDropzone } from '@/lib/uploadthing';
 import { currentUser } from '@clerk/nextjs';
@@ -12,7 +12,7 @@ const EditServer = ({ param }) => {
 
   useEffect(() => {
     fetchData();
-  },[fetchData]);
+  },[]);
 
   const getServer = async (param) => {
     try {
